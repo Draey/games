70b697bcb2838e7d2a3b8a156999a84ce65fe4f5
draesh <shabaz.draey@withsecure.com>
2022-10-20 21:41:13 +0100

    test

---
+++ web_apps/flask_proj/README.md
@@ -0,0 +1,12 @@
+# What is this Repo
+
+Flask project.
+*(wip)*
+
+## Purpose
+
+To create flask based web applications.
+
+### What is Flask
+
+Flask is used for developing web applications using python, implemented on Werkzeug and Jinja2. Advantages of using Flask framework are: There is a built-in development server and a fast debugger provided.
--- web_apps/flask_proj/flsk1.py
+++ web_apps/flask_proj/flsk1.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template 
+#url for and redirect allow return a redirect from a specific function, 
+#render grabs an html file and renders as web page
 
-app = Flask(__main__) #create instance of flask web app
+app = Flask(__name__) #create instance of flask web app
 
-@app.route("/home/parallels/Desktop/flask") #define how to access this specific page --> 
-def home():
-    return "Hello! this is the main page <h1>Hello Planet Earth<h1>"
+@app.route("/<name>") #define how to access this specific page --> 
+def home(name):
+    return render_template("index.html", content=name, r=2) #this will display the index.html page - index.html will be rendered, then varibale content of name will be replaced in the content.html file
+
+
+
+
+""""
+@app.route("/<name>") #this basically allows us to display the info below based on what we write afer the /
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin/")
+def admin():
+    return redirect(url_for("user", name="Admin!")) #When you go /admin it will reutrn url for user, and pass the arg admin
+"""
+
+
+
 
 if __name__ == "__main__":
     app.run()
+
+    #redirect different pages from our code
